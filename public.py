ad4323322425212bc1154cbf095d86a2d9c3fd16
drknsblct <drknsblct@gmail.com>
2019-12-29 20:02:01 +0200

    Initialized file

---
+++ public.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+
+url = "https://www.public.gr/cat/gaming/games/ps4/?_dyncharset=UTF-8&=undefined&Ns=sku.publicActualPrice|1&Nrpp=90"
+r = requests.get(url)
+html_content = r.text
+soup = BeautifulSoup(html_content, 'lxml')
+a = soup.findAll("div", {"class" : "col-sm-6 col-lg-4"})
+
+filename = "games.csv"
+
+with open("games.csv", "w") as f:
+    headers = "Name, Price, Availability\n"
+    f.write(headers)
+    for link in a:
+        price = link.div["data-pricerange"]
+        dia8esimothta = link.find("div", {"class":"btn btn-grey btn-as-tag"}).text.strip()
+        name = link.find("img")["alt"]
+        print(name)
+        print(price)
+        print(dia8esimothta)
+        print()
+        f.write(name + "," + price + "," + dia8esimothta + "\n")
+
