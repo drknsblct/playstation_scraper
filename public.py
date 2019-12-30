ff4e1372b38642f564c8ba78af9673f28f0263a9
drknsblct <drknsblct@gmail.com>
2019-12-30 10:48:54 +0200

    Added Url Links

--- public.py
+++ public.py
@@ -1,26 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
 
+url = "https://www.public.gr/cat/gaming/games/ps4/?_dyncharset=UTF-8&=undefined&Ns=sku.publicActualPrice|1&Nrpp=790"
 
-
-url = "https://www.public.gr/cat/gaming/games/ps4/?_dyncharset=UTF-8&=undefined&Ns=sku.publicActualPrice|1&Nrpp=90"
 r = requests.get(url)
 html_content = r.text
 soup = BeautifulSoup(html_content, 'lxml')
+
 a = soup.findAll("div", {"class" : "col-sm-6 col-lg-4"})
+
 
 filename = "games.csv"
 
-with open("games.csv", "w") as f:
-    headers = "Name, Price, Availability\n"
+with open(filename, "w") as f:
+    headers = "NAME, PRICE, AVAILABILITY, LINK\n"
     f.write(headers)
     for link in a:
+
         price = link.div["data-pricerange"]
         dia8esimothta = link.find("div", {"class":"btn btn-grey btn-as-tag"}).text.strip()
         name = link.find("img")["alt"]
+        selida = link.find("a", {"class":"product-image product-page-link istile"})["href"]
+        address = f"www.public.gr{selida}"
+
         print(name)
         print(price)
         print(dia8esimothta)
-        print()
-        f.write(name + "," + price + "," + dia8esimothta + "\n")
+        print(address)
+        f.write(name + "," + price + "," + dia8esimothta + "," + address + "\n")
 
