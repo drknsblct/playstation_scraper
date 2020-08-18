8b553750d1f74cf7465f2862b9e12c2109677305
drknsblct <drknsblct@gmail.com>
2020-08-19 02:10:41 +0300

    Deleted print statements and updated README.md

--- public.py
+++ public.py
@@ -11,20 +11,16 @@ soup = BeautifulSoup(html_content, 'lxml')
 
 a = soup.findAll("div", {"class": "col-sm-6 col-lg-4"})
 
-filename = "games.csv"
+filename = "games.csv" 
 
 with open(filename, "w") as f:
-    headers = "NAME; PRICE; AVAILABILITY; LINK\n"
+    headers = "NAME, PRICE, AVAILABILITY, LINK\n"
     f.write(headers)
     for link in a:
         price = link.div["data-pricerange"]
-        dia8esimothta = link.find("div", {"class": "btn btn-grey btn-as-tag"}).text.strip()
+        availability = link.find("div", {"class": "btn btn-grey btn-as-tag"}).text.strip()
         name = link.find("img")["alt"]
-        selida = link.find("a", {"class": "product-image product-page-link istile"})["href"]
-        address = f"www.public.gr{selida}"
-
-        print(name)
-        print(price)
-        print(dia8esimothta)
-        print(address)
-        f.write(name + ";" + price + ";" + dia8esimothta + ";" + address + "\n")
+        page = link.find("a", {"class": "product-image product-page-link istile"})["href"]
+        address = f"www.public.gr{page}"
+        f.write(name + "," + price + "," + availability + "," + address + "\n")
+print("Done!")
