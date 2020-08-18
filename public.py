547b9d1f972cf224fdd4283ffa06b935e6f8fe6d
drknsblct <drknsblct@gmail.com>
2020-08-18 22:58:50 +0300

    Added message for user

--- public.py
+++ public.py
@@ -2,14 +2,14 @@
 from bs4 import BeautifulSoup
 import requests
 
+print("Writing games to csv...")
 url = "https://www.public.gr/cat/gaming/games/ps4/?_dyncharset=UTF-8&=undefined&Ns=sku.publicActualPrice|1&Nrpp=790"
 
 r = requests.get(url)
 html_content = r.text
 soup = BeautifulSoup(html_content, 'lxml')
 
-a = soup.findAll("div", {"class" : "col-sm-6 col-lg-4"})
-
+a = soup.findAll("div", {"class": "col-sm-6 col-lg-4"})
 
 filename = "games.csv"
 
@@ -17,11 +17,10 @@ with open(filename, "w") as f:
     headers = "NAME; PRICE; AVAILABILITY; LINK\n"
     f.write(headers)
     for link in a:
-
         price = link.div["data-pricerange"]
-        dia8esimothta = link.find("div", {"class":"btn btn-grey btn-as-tag"}).text.strip()
+        dia8esimothta = link.find("div", {"class": "btn btn-grey btn-as-tag"}).text.strip()
         name = link.find("img")["alt"]
-        selida = link.find("a", {"class":"product-image product-page-link istile"})["href"]
+        selida = link.find("a", {"class": "product-image product-page-link istile"})["href"]
         address = f"www.public.gr{selida}"
 
         print(name)
@@ -29,4 +28,3 @@ with open(filename, "w") as f:
         print(dia8esimothta)
         print(address)
         f.write(name + ";" + price + ";" + dia8esimothta + ";" + address + "\n")
-
