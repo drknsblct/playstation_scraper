a2678a6e793f1c85524024e800e52f248924baba
drknsblct <drknsblct@gmail.com>
2020-11-03 20:34:00 +0200

    Change name of output csv files

--- public.py
+++ public.py
@@ -6,10 +6,12 @@ while True:
     choice = input("Do you want games for PS4 or PS5? ")
     if choice == "PS4" or choice == "ps4":
         url = "https://www.public.gr/cat/gaming/games/ps4/?_dyncharset=UTF-8&=undefined&Ns=sku.publicActualPrice|1&Nrpp=790"
+        filename = "ps4_games.csv"
         print("Writing PS4 games to csv...")
         break
     elif choice == "PS5" or choice == "ps5":
         url = "https://www.public.gr/cat/gaming/games/ps5/?_dyncharset=UTF-8&=undefined&Nrpp=790&Ns=sku.publicActualPrice|1"
+        filename = "ps5_games.csv"
         print("Writing PS5 games to csv...")
         break
     else:
@@ -19,7 +21,7 @@ r = requests.get(url)
 html_content = r.text
 soup = BeautifulSoup(html_content, 'lxml')
 a = soup.findAll("div", {"class": "col-sm-6 col-lg-4"})
-filename = "games.csv"
+
 
 
 def write_to_excel():
