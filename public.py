9ccd587718c37a7214fe454a968adfa3737a2bdf
drknsblct <drknsblct@gmail.com>
2021-02-14 04:31:54 +0200

    Added choice to scrap both playstation systems

--- public.py
+++ public.py
@@ -17,26 +17,45 @@ def write_to_excel():
 
 
 if __name__ == '__main__':
+    urls = [
+        'https://www.public.gr/cat/gaming/games/ps4/?_dyncharset=UTF-8&=undefined&Ns=sku.publicActualPrice|1&Nrpp=790',
+        'https://www.public.gr/cat/gaming/games/ps5/?_dyncharset=UTF-8&=undefined&Nrpp=790&Ns=sku.publicActualPrice|1']
+
+    csv_filenames = ['ps4_games.csv', 'ps5_games.csv']
+
     while True:
-        choice = input('List of games for PS4 or PS5? ')
+        choice = input('List of games for [PS4], [PS5] or [both]? ')
         if choice.strip().lower() == 'ps4':
-            url = 'https://www.public.gr/cat/gaming/games/ps4/?_dyncharset=UTF-8&=undefined&Ns=sku.publicActualPrice|1&Nrpp=790'
-            filename = 'ps4_games.csv'
+            url = urls[0]
+            filename = csv_filenames[0]
             print('Writing PS4 games to csv...')
             break
         elif choice.strip().lower() == 'ps5':
-            url = 'https://www.public.gr/cat/gaming/games/ps5/?_dyncharset=UTF-8&=undefined&Nrpp=790&Ns=sku.publicActualPrice|1'
-            filename = 'ps5_games.csv'
+            url = urls[0]
+            filename = csv_filenames[1]
             print('Writing PS5 games to csv...')
+            break
+        elif choice.strip().lower() == 'both':
+            for i in range(len(urls)):
+                url = urls[i]
+                filename = csv_filenames[i]
+                print('Writing Playstation games to csv...')
+                r = requests.get(url)
+                html_content = r.text
+                soup = BeautifulSoup(html_content, 'lxml')
+                a = soup.findAll('div', {'class': 'col-sm-6 col-lg-4'})
+                write_to_excel()
+            print('Done')
             break
         else:
             print('Choose either PS4 or PS5!\n')
             continue
 
-    r = requests.get(url)
-    html_content = r.text
-    soup = BeautifulSoup(html_content, 'lxml')
-    a = soup.findAll('div', {'class': 'col-sm-6 col-lg-4'})
+    if choice != 'both':
+        r = requests.get(url)
+        html_content = r.text
+        soup = BeautifulSoup(html_content, 'lxml')
+        a = soup.findAll('div', {'class': 'col-sm-6 col-lg-4'})
 
-    write_to_excel()
-    print('Done')
+        write_to_excel()
+        print('Done')
